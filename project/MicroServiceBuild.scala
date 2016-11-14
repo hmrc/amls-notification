5ed497827fd49c11e91ffeec5d5d620af5612994
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-11-14 09:25:41 +0000

    changing dependencies for mockito

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -53,7 +53,7 @@ private object AppDependencies {
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % "test"
+        "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % "test"
       )
     }.test
   }
