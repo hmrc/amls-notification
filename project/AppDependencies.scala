b058689adb2b55be57ad9b527719725b49aef613
Bhanu <177983246+bhanuparsad@users.noreply.github.com>
2026-03-16 11:24:14 +0000

    DDCYLS-8080: vulnerability fixes (#134)
    * DDCYLS-8080: vulnerability fixes
    
    * DDCYLS-8080: vulnerability fixes
    
    ---------
    
    Co-authored-by: bhanu-nov1989 <177983246+bhanu-nov1989@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,12 @@ import play.core.PlayVersion
 private object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"        %  "2.12.0",
     "uk.gov.hmrc"       %% s"bootstrap-backend-$playVersion" % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "uk.gov.hmrc"       %% "domain"                          % "8.3.0-play-28",
-    "org.apache.commons"    % "commons-lang3"                          % "3.18.0",
-    "ch.qos.logback"        % "logback-core"                           % "1.5.27",
-    "ch.qos.logback"        % "logback-classic"                        % "1.5.27",
-    "at.yawk.lz4"           %  "lz4-java"                              % "1.10.3",
     ws
   )
 
